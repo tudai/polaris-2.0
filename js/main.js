917891702c7e06eb3a22f4f3668bcf8561f68e54
Leonardo Navarro <maikndawer@gmail.com>
2015-06-19 15:46:19 -0300

    erq

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ function getServerURL(){
 	return ((location.href.split('/'))[0])+'//'+((location.href.split('/'))[2]) + "/";
 }
 
-function loadPage(id){
+function loadPage(id, target){
 	
 	$.ajax({
 		method: 'GET',
 		url: getServerURL() + 'polaris-2.0/' + id + '.html',
 		dataType: 'html',
 		success: function(data){
-			$('#content').html(data);
+			$('#'+target).html(data);
 		},
 		error: function(){
 			alert('se produjo un error de red, wachin');
@@ -24,19 +24,13 @@ function loadPage(id){
 
 
 $(function(){
-	$('#content').load("sections.html #home");
 	$('nav li > a').click(function(event){
 		event.preventDefault();
-		loadPage($(this).attr('id'));
+		loadPage($(this).attr('id'), 'content');
 	})
 	
-	$('#nav ').click()
+	$('#home').click();
 
-	
-	$('nav li > a').click(function(event){
-		event.preventDefault();
-		$('#content').load("sections.html #"+$(this).attr('id'));
-	})
 
 })
 
